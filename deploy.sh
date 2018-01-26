d68c58651c6057b19f11f0527d5ea9664b832b86
Herve Couplet <herve.couplet@creativeone.eu>
2018-01-27 00:21:52 +0100

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,4 @@ docker-compose up -d consul-config-loader
 echo "Waiting 10s for consul config"
 sleep 10
 docker-compose up -d app-mariadb
-echo "Waiting 60s for uaa to be started"
-sleep 10
 docker-compose up -d
